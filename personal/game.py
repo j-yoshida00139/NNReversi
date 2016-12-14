513916912b9f0e775dd0039857cf5723e3b38ca2
sample user <sample@sample.com>
2016-12-14 18:49:10 +0100

    add auto playing

---
+++ personal/game.py
@@ -0,0 +1,237 @@
+import math
+import sys, os
+import csv
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+
+
+directions = [
+    {"row": 0, "col": 1},
+    {"row":-1, "col": 1},
+    {"row":-1, "col": 0},
+    {"row":-1, "col":-1},
+    {"row": 0, "col":-1},
+    {"row": 1, "col":-1},
+    {"row": 1, "col": 0},
+    {"row": 1, "col": 1}
+]
+
+class Game(object):
+    def __init__(self,rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.NONE  = 0
+        self.BLACK = 1
+        self.WHITE = 2
+        self.nextColor = self.BLACK
+        self.blackMove = []
+        self.whiteMove = []
+
+    def initialize(self):
+        upRow   = math.floor((self.rows-1)/2)
+        leftCol = math.floor((self.cols-1)/2)
+        self.arrange = [[0 for col in range(0, self.cols)] for row in range(0, self.rows)]
+
+        self.putPiece(upRow  , leftCol  , self.BLACK)
+        self.putPiece(upRow+1, leftCol  , self.WHITE)
+        self.putPiece(upRow  , leftCol+1, self.WHITE)
+        self.putPiece(upRow+1, leftCol+1, self.BLACK)
+
+    def clearPiece(self, row, col):
+        self.arrange[row][col] = self.NONE
+
+    def putPiece(self, row, col, color):
+        self.arrange[row][col] = color
+
+    def canPutPiece(self, row, col, color):
+        if self.arrange[row][col]!=self.NONE:
+            return False
+        if len(self.getTurnPieceList(row, col, color))>0:
+            return True
+        return False
+
+    def storeMove(self, row, col, color):
+        if color==self.BLACK :
+            self.blackMove.append({"arrange":self.arrange, "color":color, "row":row, "col":col})
+        else:
+            self.whiteMove.append({"arrange":self.arrange, "color":color, "row":row, "col":col})
+
+    def isOutOfRange(self, row, col):
+        if row>=self.rows or col>=self.cols or row<0 or col<0:
+            return True
+        else:
+            return False
+
+    def getScore(self, color):
+        counter = 0
+        for row in range(0, self.rows):
+            for col in range(0, self.cols):
+                if self.arrange[row][col]==color:
+                    counter+=1
+        return counter
+
+    def getTurnPieceList(self, row, col, color):
+        turnPieceList = []
+        for i in range(0, len(directions)):
+            tmpTurnPieceList = self.getTurnPieceForDirect(row, col, color,  directions[i]['row'], directions[i]['col'])
+            if len(tmpTurnPieceList)>0:
+                for j in range(0, len(tmpTurnPieceList)):
+                    turnPieceList.append({"row":tmpTurnPieceList[j]['row'], "col":tmpTurnPieceList[j]['col']})
+        return turnPieceList
+
+    def getTurnPieceForDirect(self, row, col, color, y, x):
+        turnPieceList = []
+        if self.isOutOfRange(row+y, col+x):
+            return []
+
+        checkRow = row+y
+        checkCol = col+x
+        if self.arrange[checkRow][checkCol]==color or self.arrange[checkRow][checkCol]==self.NONE:
+            return []
+
+        turnRows = []
+        turnCols = []
+        turnRows.append(checkRow)
+        turnCols.append(checkCol)
+        checkRow += y
+        checkCol += x
+
+        while not(self.isOutOfRange(checkRow, checkCol)):
+            if self.arrange[checkRow][checkCol]==self.NONE:
+                return []
+            elif self.arrange[checkRow][checkCol]==color:
+                for i in range(0, len(turnRows)):
+                    turnPieceList.append({"row":turnRows[i], "col":turnCols[i]})
+                return turnPieceList
+            turnRows.append(checkRow)
+            turnCols.append(checkCol)
+            checkRow += y
+            checkCol += x
+        return turnPieceList
+
+    def canPutPieceOnBoard(self, color):
+        canPutList = self.getCanPutList(color)
+        for i in range(0, len(canPutList)):
+            if canPutList[i]==1:
+                return True
+        return False
+
+    def getCanPutList(self, color):
+        canPutList = []
+        for y in range(0, self.rows):
+            for x in range(0, self.cols):
+                if self.canPutPiece(y, x, color):
+                    canPutList.append(1)
+                else:
+                    canPutList.append(0)
+        return canPutList
+
+    def getWinnersData(self):
+        if self.getScore(self.BLACK) > self.getScore(self.WHITE):
+            return self.blackMove
+        else:
+            return self.whiteMove
+
+    def goNextTurn(self):
+        self.nextColor = self.WHITE if self.nextColor==self.BLACK else self.BLACK
+        if self.canPutPieceOnBoard(self.nextColor):
+            return True
+        else:
+            self.nextColor = self.WHITE if self.nextColor == self.BLACK else self.BLACK
+            if self.canPutPieceOnBoard(self.nextColor):
+                return True
+            else:
+                winnersData = self.getWinnersData()
+#                print(winnersData)
+#                self.sendWinnersData(winnersData)
+                return False
+
+
+
+    def turnPiece(self, turnPieceList, color):
+        for i in range(0, len(turnPieceList)):
+            self.putPiece(turnPieceList[i]["row"], turnPieceList[i]["col"], color)
+
+    def returnNnInputList(self, rawArray, color):
+        arrangeList = []
+        for cols in rawArray:
+            for value in cols:
+                if value == 0:
+                    arrangeList.append([float(1)])
+                    arrangeList.append([float(0)])
+                    arrangeList.append([float(0)])
+                elif value == color:
+                    arrangeList.append([float(0)])
+                    arrangeList.append([float(1)])
+                    arrangeList.append([float(0)])
+                else:
+                    arrangeList.append([float(0)])
+                    arrangeList.append([float(0)])
+                    arrangeList.append([float(1)])
+        return arrangeList
+
+    def storeWinnersData(self, winnersData):
+        winnersData
+        print (winnersData)
+        lastFileNo = self.getLastFileNo()
+
+        for i in range(len(winnersData)):
+            winnersMove = winnersData[i]
+            # winnersMove = json.loads(winnersDataArray[i])
+            inputList = self.returnNnInputList(winnersMove["arrange"], winnersMove["color"])
+            fileNo = lastFileNo + i
+            fileNameInput = os.path.dirname(os.path.abspath(__file__)) + "/nncore/winnersData/input_{0:08d}".format(
+                fileNo + 1)
+            fileNameOutput = os.path.dirname(
+                os.path.abspath(__file__)) + "/nncore/winnersData/output_{0:08d}".format(fileNo + 1)
+            fIn = open(fileNameInput + '.csv', 'w')
+            fOut = open(fileNameOutput + '.csv', 'w')
+
+            row = winnersMove["row"]
+            col = winnersMove["col"]
+            moveList = self.returnMoveList(row, col)
+
+            dataWriterIn = csv.writer(fIn)
+            # for i in range(0, len(inputList)):
+            dataWriterIn.writerow(self.returnNnInputStoreList(inputList))
+            fIn.close()
+            dataWriterOut = csv.writer(fOut)
+            # for i in range(0, len(moveList)):
+            dataWriterOut.writerow(moveList)
+            fOut.close()
+
+        return True
+
+
+    def getLastFileNo(self):
+        path = os.path.dirname(os.path.abspath(__file__)) + "/nncore/winnersData/"
+        fileNoList = [self.retrieveFileNo(f) for f in listdir(path) if isfile(join(path, f))]
+        return np.max(fileNoList)
+
+    def returnMoveList(self, row, col):
+        moveList = []
+        for y in range(0, self.rows):
+            for x in range(0, self.cols):
+                if y==row and x==col:
+                    moveList.append(float(1))
+                else:
+                    moveList.append(float(0))
+        return moveList
+
+    def returnNnInputStoreList(self, rawArray):
+        arrangeList = []
+        for value in rawArray:
+            arrangeList.append(value[0])
+        print(arrangeList)
+        return arrangeList
+
+    def retrieveFileNo(self, filename):
+        print(filename)
+        if filename.count(".csv"):
+            tmpFileName = filename.replace("input_", "")
+            tmpFileName = tmpFileName.replace("output_", "")
+            tmpFileName = tmpFileName.replace(".csv", "")
+            return int(tmpFileName)
+        else:
+            return 0
---
+++ personal/playAuto.py
@@ -0,0 +1,37 @@
+import sys, os
+import game
+import numpy as np
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/nncore')
+import network
+
+n_input = 192 #366
+n_neutral_neuron = 100
+n_output = 64 #12
+
+size = [n_input, n_neutral_neuron, n_output]
+net = network.Network(size)
+
+game = game.Game(8,8)
+game.initialize()
+
+while game.canPutPieceOnBoard(game.WHITE) or game.canPutPieceOnBoard(game.BLACK):
+    arrangeList = game.returnNnInputList(game.arrange, game.nextColor)
+    move = net.feedforward(arrangeList) #move[0][0:63]
+#    print(move)
+#    print("result")
+#    print(type(result))
+#    rand = np.random.random(64)
+#    print("rand")
+#    print(type(rand))
+    index = np.argmax(move[0]*game.getCanPutList(game.nextColor))
+    row, col = divmod(index, 8)
+
+    game.storeMove(row, col, game.nextColor)
+    game.putPiece(row, col, game.nextColor)
+
+    turnPieceList = game.getTurnPieceList(row, col, game.nextColor)
+    game.turnPiece(turnPieceList, game.nextColor)
+    game.goNextTurn()
+
+print(game.getWinnersData())
+game.storeWinnersData(game.getWinnersData())
