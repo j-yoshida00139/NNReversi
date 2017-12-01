e2da989c8a90e4f8ffa4f6f1c085df19694cb93f
sample user <sample@sample.com>
2017-12-01 07:57:52 +0100

    Merge CreateNNCoreApp

---
+++ django/personal/management/commands/postBestMove.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from django.core.management.base import BaseCommand
+from ...models import BestMove
+from ...utils import basicFunc, game
+import random
+import requests, json
+
+# numGames = 500
+numGames = 10
+
+
+class Command(BaseCommand):
+	def handle(self, *args, **kwargs):
+		bestMoves = BestMove.objects.all()
+		for bestMove in bestMoves:
+			url = 'http://nnreversi.tgen.jp.net/api/best_moves/'
+			# url = 'http://localhost:8000/api/best_moves/'
+			data = {
+				"first_half_arrangement": bestMove.first_half_arrangement,
+				"last_half_arrangement": bestMove.last_half_arrangement,
+				"move_index": bestMove.move_index
+			}
+			# data = json.load(data)
+
+			# payload = json.load(open("request.json"))
+			headers = {'content-type':'application/json', 'Accept-Charset':'UTF-8'}
+			r = requests.post(url, json=data, headers=headers)
+			# r = requests.post(url, data=json.dumps(data), headers=headers)
+
+	# num_win = 0
+		# for i in range(0, numGames):
+		# 	main_game = game.Game(8, 8)
+		# 	main_game.initialize()
+		# 	while not main_game.is_ended():
+		# 		game_arrange = BestMove.conv_to_game_arrange(main_game.arrange, main_game.nextColor)
+		# 		if not BestMove.has_move_data(game_arrange):
+		# 			best_row, best_col, win_ratio = main_game.find_best_move()
+		# 			print("Best move is row:%d, col:%d, nextColor:%d, win ratio:%d %s" % (
+		# 				best_row, best_col, main_game.nextColor, win_ratio, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+		# 			tmp_arrange_list = basicFunc.unshared_copy(main_game.arrange)
+		# 			winners_move = {"arrange": tmp_arrange_lisvim t, "color": main_game.nextColor, "row": best_row, "col": best_col}
+		# 			BestMove.store_best_move(winners_move)
+		# 		else:
+		# 			print("Best move is already exist.")
+		# 		main_game.go_next_with_auto_move(random.choice([True, False]))
+		# 	print("%s games was finished. BLACK:%d WHITE:%d" % (
+		# 		i, main_game.get_score(main_game.BLACK), main_game.get_score(main_game.WHITE)))
+		#
+		# 	if main_game.get_score(main_game.BLACK) > main_game.get_score(main_game.WHITE):
+		# 		num_win += 1
+		# print("Win : %s" % num_win)
