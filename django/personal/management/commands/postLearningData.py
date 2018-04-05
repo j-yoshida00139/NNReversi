830791272ea2f618be2c81a170f5ab36b5f39eb0
Jun Yoshida <yoshida@YMBP2017.local>
2018-04-05 22:45:26 +0200

    Add tensorflow training code

--- django/personal/management/commands/postLearningData.py
+++ django/personal/management/commands/postLearningData.py
@@ -5,6 +5,7 @@ from ...models import BestMove
 import math
 import requests
 import json
+import pickle
 
 
 class Command(BaseCommand):
@@ -14,17 +15,23 @@ class Command(BaseCommand):
 		print(n_batch_size)
 
 		x_train, t_train, x_eva, t_eva, x_test, t_test = move_loader.load_data(n_batch_size, flatten=False)
-		url = config.ENDPOINT_URL + "/nncore/upload_input/"
-		payload = {
-			"x_train": x_train.tolist(),
-			"t_train": t_train.tolist(),
-			"x_test": x_test.tolist(),
-			"t_test": t_test.tolist()}
-		headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-		response = requests.post(url, data=json.dumps(payload), headers=headers)
-		print(url)
-		print(response.status_code)
-		if response.status_code == 200:
-			print("The data is stored.")
-		else:
-			print("Error. Check the logs.")
+		params = dict()
+		params["x_train"], params["t_train"], params["x_test"], params["t_test"] = \
+			x_train, t_train, x_test, t_test
+		with open("nncore/input_data/learn_input.pkl", 'wb') as f:
+			pickle.dump(params, f)
+
+		# url = config.ENDPOINT_URL + "/nncore/upload_input/"
+		# payload = {
+		# 	"x_train": x_train.tolist(),
+		# 	"t_train": t_train.tolist(),
+		# 	"x_test": x_test.tolist(),
+		# 	"t_test": t_test.tolist()}
+		# headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+		# response = requests.post(url, data=json.dumps(payload), headers=headers)
+		# print(url)
+		# print(response.status_code)
+		# if response.status_code == 200:
+		# 	print("The data is stored.")
+		# else:
+		# 	print("Error. Check the logs.")
